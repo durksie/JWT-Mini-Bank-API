112e2da22ade68b398409069e6c0a59713391f0f
Durksie <durksie1@gmail.com>
2026-02-20 09:13:30 +0200

    Spring initial structure

---
+++ src/main/java/com/minibank/JWT/Mini/Bank/API/JwtMiniBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.minibank.JWT.Mini.Bank.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMiniBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtMiniBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minibank/JWT/Mini/Bank/API/JwtMiniBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.JWT.Mini.Bank.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtMiniBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
